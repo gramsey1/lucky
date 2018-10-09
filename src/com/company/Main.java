4d65c2e306f3715e29921739b4ea68fd2f3ff196
gramsey1 <graceramsey312@yahoo.com>
2018-10-09 11:18:43 -0400

    idk how to code at all. help me please.

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("What is your name");
+        Scanner kbInput1 = new Scanner(System.in);
+        String userInput = kbInput1.next();
+    }
+}
---
+++ src/com/company/number.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by gr236 on 10/9/18.
+ */
+public class number {
+}
