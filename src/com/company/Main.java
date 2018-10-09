44afba20691692e2fc93a991d3cf4bcb92a8ca2d
gramsey1 <graceramsey312@yahoo.com>
2018-10-09 16:07:48 -0400

    i hate coding

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,12 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("What is your name");
-        Scanner kbInput1 = new Scanner(System.in);
-        String userInput = kbInput1.next();
+        int numStudents = 1;
+        String name[] = new String[numStudents];
+     for (int j=0; j < numStudents; j++) {
+         Scanner kbReader1 = new Scanner(System.in);
+         System.out.println("What is your name");
+         name[j] = kbReader1.nextLine();
+     }
     }
 }
--- src/com/company/number.java
+++ src/com/company/number.java
@@ -1,7 +1,14 @@
 package com.company;
+import java.util.Random;
 
 /**
  * Created by gr236 on 10/9/18.
  */
 public class number {
-}
+
+    public static double perfectRandom(){
+        double x = Math.random();
+        return x;
+    }
+    }
+
